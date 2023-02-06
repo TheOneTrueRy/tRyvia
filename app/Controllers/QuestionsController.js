0d23130052f14a4dd045aa8f0ef98dd99ea79f4d
Ryan Wittman <OfficiallyRy@outlook.com>
2023-02-06 16:50:53 -0700

    laptop state

--- app/Controllers/QuestionsController.js
+++ app/Controllers/QuestionsController.js
@@ -1,14 +1,21 @@
 import { appState } from "../AppState.js";
 import { questionsService } from "../Services/QuestionsService.js";
 
+function _drawQuestion(){
+
+}
 
 export class QuestionsController{
   constructor(){
-
+    this.getQuestions()
   }
 
   async getQuestions(){
     await questionsService.getQuestions()
     console.log(appState.questions)
+  }
+
+  startGame(){
+    
   }
 }
